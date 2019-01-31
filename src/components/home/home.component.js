f49d60784ecba56d476913447a30196876ead6f9
unknown <abhishekguptaatweb17@gmail.com>
2019-01-31 11:22:31 +0530

    add mainView

--- src/components/home/home.component.js
+++ src/components/home/home.component.js
@@ -2,18 +2,22 @@ import React from 'react'
 import './home.style.css'
 import Sidebar from '../sidebar/sidebar.component'
 import { useWindowWidth } from '../../hooks'
+import MainView from '../mainView';
+const Home = ({ load }) => {
 
-const Home = () => {
   const width = useWindowWidth()
   const isMobile = width < 757
   return (
+
     <>
       <Sidebar isMobile={isMobile} />
+
       <div
         className="Content"
         style={isMobile ? { paddingLeft: 0 } : { paddingLeft: 220 }}
       >
-        Content
+        <MainView />
+
       </div>
     </>
   )
