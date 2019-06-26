242e3523d9da7e5a1021bcf02b4cd3be6ae28511
Amit Kushwaha <amitbvp13@gmail.com>
2019-06-26 12:36:38 +0530

    Added link to category page

--- src/components/home/categoryWrapper.container.js
+++ src/components/home/categoryWrapper.container.js
@@ -1,34 +1,39 @@
 import React from 'react'
 import { Card, Row, Col } from 'antd'
 import './home.style.css'
+import { Link } from 'gatsby'
 import categoryList from '../../list.json'
 const { Meta } = Card
 
-const category = Object.values(categoryList)
+const category = Object.keys(categoryList)
 
 const CategoryWrapper = props => {
   return (
     <React.Fragment>
       <Row gutter={16}>
-        {category.map(item => (
-          <Col span={8}>
-            <div style={{ display: 'flex', position: 'relative' }}>
-              <Card
-                hoverable
-                style={{
-                  width: 300,
-                  height: 80,
-                  overflow: 'scroll',
-                }}
-              >
-                <Meta title={item.name} />
-              </Card>
-            </div>
-          </Col>
-        ))}
+        {category.map(categoryKey => {
+          let categoryValue = categoryList[categoryKey]
+          return (
+            <Col span={8}>
+              <div style={{ display: 'flex', position: 'relative' }}>
+                <Card
+                  hoverable
+                  style={{
+                    width: 300,
+                    height: 80,
+                    overflow: 'scroll',
+                  }}
+                >
+                  <Link to={`/${categoryKey}`}>
+                    <Meta title={categoryValue.name} />
+                  </Link>
+                </Card>
+              </div>
+            </Col>
+          )
+        })}
         <br />
       </Row>
-      {console.log(category)}
     </React.Fragment>
   )
 }
