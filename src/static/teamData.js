59ecf777e77e0352511e6b5d11afe0ef735c4736
Amit Kushwaha <amitbvp13@gmail.com>
2019-06-30 23:32:41 +0530

    Added Linkedin and github links

---
+++ src/static/teamData.js
@@ -0,0 +1,45 @@
+const teamData = [
+  {
+    name: 'Prakritidev Verma',
+    description:
+      "Data Scientist | AWS Cloud Architect | SAP Successfactor's Consultant",
+    profileImage:
+      'https://media.licdn.com/dms/image/C4E03AQGRMkcNREZYfw/profile-displayphoto-shrink_200_200/0?e=1565222400&v=beta&t=xX6HNnXoQnvvdP6eKwCcxOQMcCK4woljlcrnGHKxK7s',
+    linkedInUrl: 'https://www.linkedin.com/in/prakritidevverma',
+    githubUrl: 'https://github.com/prakritidev',
+  },
+  {
+    name: 'Tushar Mudgal',
+    description: 'Building scalable systems for high frequency data.',
+    profileImage:
+      'https://media.licdn.com/dms/image/C5103AQHzGKS49lcoqA/profile-displayphoto-shrink_800_800/0?e=1565222400&v=beta&t=BAqezEJAEAWJdH95LIFhFJu4h-XX1SgXRwHXTRSpeak',
+    linkedInUrl: 'https://www.linkedin.com/in/tusharmudgal3',
+    githubUrl: 'https://github.com/mr-woot',
+  },
+  {
+    name: 'Amit Kushwaha',
+    description: 'UI Engineer',
+    profileImage:
+      'https://media.licdn.com/dms/image/C5603AQH2tH2jwN0RXg/profile-displayphoto-shrink_800_800/0?e=1565222400&v=beta&t=IsJC9Rym6K0DGK1HHXFst5Gshry0HkSx6OH6L2FfveE',
+    linkedInUrl: 'https://www.linkedin.com/in/amit-kushwaha-755704106',
+    githubUrl: 'https://github.com/devilcrucifier',
+  },
+  {
+    name: 'Waqas Hamid',
+    description: 'Software Development Engineer',
+    profileImage:
+      'https://media.licdn.com/dms/image/C5603AQEvoBs0R1zfPQ/profile-displayphoto-shrink_800_800/0?e=1565222400&v=beta&t=lbg_JQO_-RvM_fBwbmKVweIrV3TRMm_aIgQ-g_dA_Ak',
+    linkedInUrl: 'https://www.linkedin.com/in/hamidwaqas',
+    githubUrl: 'https://github.com/waqashamid',
+  },
+  {
+    name: 'Mohit',
+    description:
+      'Pursuing M.C.A from G.G.S.I.P.U. currently invested in back-end development with tools like Nodejs, Expressjs and MongoDb',
+    profileImage: 'https://avatars2.githubusercontent.com/u/15038734?s=400&v=4',
+    linkedInUrl: 'https://www.linkedin.com/in/mohit-kumar-868739184',
+    githubUrl: 'https://github.com/mhtkmr',
+  },
+]
+
+export default teamData
