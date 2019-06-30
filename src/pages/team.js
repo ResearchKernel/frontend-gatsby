6719345d0f51fe95742888d31c2d63e1c903c41d
Amit Kushwaha <amitbvp13@gmail.com>
2019-06-30 23:32:02 +0530

    Added team data object

--- src/pages/team.js
+++ src/pages/team.js
@@ -2,10 +2,11 @@ import React from 'react'
 import Header from '../components/header/header'
 import { Card, Row, Col } from 'antd'
 import CardWrapper from '../components/card/card.component'
+import teamData from '../static/teamData'
 
 const { Meta } = Card
 
-function team() {
+function Team() {
   return (
     <div>
       <Header />
@@ -29,53 +30,25 @@ function team() {
         }}
       >
         <Row gutter={16}>
-          <Col span={8}>
-            <CardWrapper
-              altImage="example"
-              source="https://media.licdn.com/dms/image/C4E03AQGRMkcNREZYfw/profile-displayphoto-shrink_200_200/0?e=1565222400&v=beta&t=xX6HNnXoQnvvdP6eKwCcxOQMcCK4woljlcrnGHKxK7s"
-              title="Prakritidev Verma "
-              description="Data Scientist | AWS Cloud Architect | SAP Successfactor's Consultant"
-            />
-          </Col>
-          <Col span={8}>
-            <CardWrapper
-              altImage="example"
-              source="https://media.licdn.com/dms/image/C5103AQHzGKS49lcoqA/profile-displayphoto-shrink_800_800/0?e=1565222400&v=beta&t=BAqezEJAEAWJdH95LIFhFJu4h-XX1SgXRwHXTRSpeak"
-              title="Tushar Mudgal "
-              description="Building scalable systems for high frequency data."
-            />
-          </Col>
-          <Col span={8}>
-            <CardWrapper
-              altImage="example"
-              source="https://media.licdn.com/dms/image/C5603AQH2tH2jwN0RXg/profile-displayphoto-shrink_800_800/0?e=1565222400&v=beta&t=IsJC9Rym6K0DGK1HHXFst5Gshry0HkSx6OH6L2FfveE"
-              title="Amit Kushwaha"
-              description="UI Engineer"
-            />
-          </Col>
-        </Row>
-        <br />
-        <Row gutter={16}>
-          <Col span={8}>
-            <CardWrapper
-              altImage="example"
-              source="https://media.licdn.com/dms/image/C5603AQEvoBs0R1zfPQ/profile-displayphoto-shrink_800_800/0?e=1565222400&v=beta&t=lbg_JQO_-RvM_fBwbmKVweIrV3TRMm_aIgQ-g_dA_Ak"
-              title="Waqas Hamid"
-              description="Software Development Engineer"
-            />
-          </Col>
-          <Col span={8}>
-            <CardWrapper
-              altImage="example"
-              source="https://avatars2.githubusercontent.com/u/15038734?s=400&v=4"
-              title="Mohit "
-              description="Pursuing M.C.A from G.G.S.I.P.U. currently invested in back-end development with tools like Nodejs, Expressjs and MongoDb"
-            />
-          </Col>
+          {teamData.map(
+            ({ profileImage, name, description, linkedInUrl, githubUrl }) => (
+              <Col span={8}>
+                <CardWrapper
+                  altImage={name}
+                  source={profileImage}
+                  title={name}
+                  description={description}
+                  linkedInUrl={linkedInUrl}
+                  githubUrl={githubUrl}
+                />
+                <br />
+              </Col>
+            )
+          )}
         </Row>
       </div>
     </div>
   )
 }
 
-export default team
+export default Team
