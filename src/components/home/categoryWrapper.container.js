e6d9ebf7bfd255a742a56442d5ef9d56ca4a8bc6
Amit Kushwaha <amitbvp13@gmail.com>
2019-07-03 17:11:28 +0530

    Updated category Cards

--- src/components/home/categoryWrapper.container.js
+++ src/components/home/categoryWrapper.container.js
@@ -1,44 +1,29 @@
 import React from 'react'
-import { Card, Row, Col } from 'antd'
 import './home.style.css'
 import { Link } from 'gatsby'
 import categoryList from '../../list.json'
-const { Meta } = Card
-
-const category = Object.keys(categoryList)
+import { CategoryCarousal, CategoryCard } from './categoryWrapper.style'
+const { Meta } = CategoryCard
 
 const CategoryWrapper = props => {
+  const category = Object.keys(categoryList)
   return (
-    <React.Fragment>
-      <Row gutter={16}>
-        {category.map(categoryKey => {
-          let categoryValue = categoryList[categoryKey]
-          return (
-            <Col span={8}>
-              <div style={{ display: 'flex', position: 'relative' }}>
-                <Card
-                  hoverable
-                  style={{
-                    width: 300,
-                    height: 80,
-                    overflow: 'scroll',
-                  }}
-                >
-                  <Link
-                    key={categoryKey}
-                    to={`/category?key=${categoryKey}`}
-                    state={{ categoryKey: categoryKey }}
-                  >
-                    <Meta title={categoryValue.name} />
-                  </Link>
-                </Card>
-              </div>
-            </Col>
-          )
-        })}
-        <br />
-      </Row>
-    </React.Fragment>
+    <CategoryCarousal>
+      {category.map(categoryKey => {
+        let categoryValue = categoryList[categoryKey]
+        return (
+          <Link
+            key={categoryKey}
+            to={`/category?key=${categoryKey}`}
+            state={{ categoryKey: categoryKey }}
+          >
+            <CategoryCard hoverable>
+              <Meta title={categoryValue.name} />
+            </CategoryCard>
+          </Link>
+        )
+      })}
+    </CategoryCarousal>
   )
 }
 
