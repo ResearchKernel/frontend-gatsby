7ad8d3fa0c9775b72f14e095b34209892ded2799
Prakritidev Verma <prakritidevverma@gmail.com>
2019-07-29 23:02:50 +0530

    Added Login pages

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -4,80 +4,7 @@ import React from 'react'
 
 const { Header, Content, Footer } = Layout
 
-const CollectionCreateForm = Form.create({ name: 'form_in_modal' })(
-  // eslint-disable-next-line
-  class extends React.Component {
-    render() {
-      const { visible, onCancel, onCreate, form } = this.props
-      const { getFieldDecorator } = form
-      return (
-        <Modal
-          visible={visible}
-          title="Get inside Research Kernel"
-          okText="Create"
-          onCancel={onCancel}
-          onOk={onCreate}
-        >
-          <Form layout="vertical">
-            <Form.Item label="Title">
-              {getFieldDecorator('title', {
-                rules: [
-                  {
-                    required: true,
-                    message: 'Please input the title of collection!',
-                  },
-                ],
-              })(<Input />)}
-            </Form.Item>
-            <Form.Item label="Description">
-              {getFieldDecorator('description')(<Input type="textarea" />)}
-            </Form.Item>
-            <Form.Item className="collection-create-form_last-form-item">
-              {getFieldDecorator('modifier', {
-                initialValue: 'public',
-              })(
-                <Radio.Group>
-                  <Radio value="public">Public</Radio>
-                  <Radio value="private">Private</Radio>
-                </Radio.Group>
-              )}
-            </Form.Item>
-          </Form>
-        </Modal>
-      )
-    }
-  }
-)
-
 class header extends React.Component {
-  state = {
-    visible: false,
-  }
-
-  showModal = () => {
-    this.setState({ visible: true })
-  }
-
-  handleCancel = () => {
-    this.setState({ visible: false })
-  }
-
-  handleCreate = () => {
-    const form = this.formRef.props.form
-    form.validateFields((err, values) => {
-      if (err) {
-        return
-      }
-
-      console.log('Received values of form: ', values)
-      form.resetFields()
-      this.setState({ visible: false })
-    })
-  }
-
-  saveFormRef = formRef => {
-    this.formRef = formRef
-  }
   render() {
     return (
       <div>
@@ -106,26 +33,10 @@ class header extends React.Component {
             <Link to="/blog">Blog</Link>
           </Menu.Item>
           <Menu.Item style={{ float: 'right' }}>
-            <Button type="primary" onClick={this.showModal}>
-              Sign Up
-            </Button>
-            <CollectionCreateForm
-              wrappedComponentRef={this.saveFormRef}
-              visible={this.state.visible}
-              onCancel={this.handleCancel}
-              onCreate={this.handleCreate}
-            />
+            <Link to="/signup">Sign Up</Link>
           </Menu.Item>
           <Menu.Item style={{ float: 'right' }}>
-            <Button type="primary" onClick={this.showModal}>
-              Log In
-            </Button>
-            <CollectionCreateForm
-              wrappedComponentRef={this.saveFormRef}
-              visible={this.state.visible}
-              onCancel={this.handleCancel}
-              onCreate={this.handleCreate}
-            />
+            <Link to="/login">Login</Link>
           </Menu.Item>
         </Menu>
         {/* </Header> */}
---
+++ src/pages/login.js
@@ -0,0 +1,79 @@
+import { Form, Icon, Input, Button, Checkbox } from 'antd'
+import React, { Component } from 'react'
+import { Row, Layout } from 'antd'
+import Header from '../components/header/header'
+
+const { Content, Footer } = Layout
+
+export class login extends Component {
+  handleSubmit = e => {
+    e.preventDefault()
+    this.props.form.validateFields((err, values) => {
+      if (!err) {
+        console.log('Received values of form: ', values)
+      }
+    })
+  }
+  render() {
+    const { getFieldDecorator } = this.props.form
+    return (
+      <div>
+        <Header />
+        <Content style={{ padding: '0 50px', marginTop: 64 }}>
+          <Form onSubmit={this.handleSubmit} className="login-form">
+            <Form.Item>
+              {getFieldDecorator('username', {
+                rules: [
+                  { required: true, message: 'Please input your username!' },
+                ],
+              })(
+                <Input
+                  prefix={
+                    <Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />
+                  }
+                  placeholder="Username"
+                />
+              )}
+            </Form.Item>
+            <Form.Item>
+              {getFieldDecorator('password', {
+                rules: [
+                  { required: true, message: 'Please input your Password!' },
+                ],
+              })(
+                <Input
+                  prefix={
+                    <Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />
+                  }
+                  type="password"
+                  placeholder="Password"
+                />
+              )}
+            </Form.Item>
+            <Form.Item>
+              {getFieldDecorator('remember', {
+                valuePropName: 'checked',
+                initialValue: true,
+              })(<Checkbox>Remember me</Checkbox>)}
+              <a className="login-form-forgot" href="">
+                Forgot password
+              </a>
+              <Button
+                type="primary"
+                htmlType="submit"
+                className="login-form-button"
+              >
+                Log in
+              </Button>
+              Or <a href="">register now!</a>
+            </Form.Item>
+          </Form>
+        </Content>
+        <Footer>This is footer</Footer>
+      </div>
+    )
+  }
+}
+const WrappedNormalLoginForm = Form.create({ name: 'normal_login' })(login)
+
+export default WrappedNormalLoginForm
